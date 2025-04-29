eadf1a29bc4f3aeabb3014b81720a4f5cec40635
gordon zhu <gordonz@bu.edu>
2025-04-28 20:32:00 -0400

    with confidence levels

--- food-detector.py
+++ food-detector.py
@@ -11,27 +11,31 @@ from skimage.io import imread
 image_jpg = input("Enter the path to the jpg file: ")
 labelmap = "aiy_food_V1_labelmap.csv"
 input_shape = (224, 224)
+confidence_threshold = 0.5
+classes = list(pd.read_csv(labelmap)["name"])
 
-try:
-	# Load image from file path
-	image = np.asarray(imread(image_jpg), dtype="float")
-	# Resize the image
-	image = cv2.resize(image, dsize=input_shape, interpolation=cv2.INTER_CUBIC)
-	# Scale values to [0,1]
-	image = image / image.max()
-	# Model expects an input of (?,224,224,3)
-	images = np.expand_dims(image,0)
+# Load image from file path
+image = np.asarray(io.imread(image_jpg))
+# Resize the image
+image = cv2.resize(image, dsize=input_shape, interpolation=cv2.INTER_CUBIC)
+# Scale values to [0,1]
+image = image / image.max()
+# Model expects an input of (?,224,224,3)
+images = np.expand_dims(image, 0)
 
-	# Use model to predict food
-	output = model(images)
-	# Find the index
-	predicted_index = output.numpy().argmax()
-	# Read labelmap
-	classes = list(pd.read_csv(labelmap)["name"])
-	# Print prediction
-	print("Prediction : ", classes[predicted_index])
+# Use model to predict food
+output = model(images)
+# Predicted probabilities for each class
+probabilities = output.numpy()[0]
+# Predicted class index
+predicted_index = np.argmax(probabilities)
+# Predicted confidence level
+confidence_level = probabilities[predicted_index]
 
-except FileNotFoundError:
-	print(f"Error: File not found at path: {image_jpg}")
-except Exception as e:
-	print(f"Error: Unexpected: {e}")
+# Print prediction
+if confidence_level >= confidence_threshold:
+    print("Prediction: ", classes[predicted_index])
+    print("Confidence Level: ", confidence_level)
+else:
+    print("Not food")
+    print("Confidence Level: ", confidence_level)
